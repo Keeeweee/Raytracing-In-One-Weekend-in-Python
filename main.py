1b15e3eb17e75f0dd42195c6b7c3c9cc44921ab4
Keewee <epsilonmajorquezero@gmail.com>
2019-05-09 01:27:28 +0200

    Add function to paint blue gradient

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 from pyrr import Vector3 as vec3
 from PpmDrawer import PpmDrawer
+from Ray import Ray
 
 
 def scaleColor(color) -> int:
     return int(255.99 * color)
+
+
+def blueBlend(ray: Ray) -> vec3:
+    unit_direction = ray.direction().normalized
+
+    t = 0.5 * unit_direction.y + 1.0
+
+    return (1.0 - t) * vec3([1.0, 1.0, 1.0]) + t * vec3([0.5, 0.7, 1.0])
+
+
+def paintBlueGradient():
+    nx = 200
+    ny = 100
+    ppmDrawer = PpmDrawer("blue_gradient.ppm", nx, ny)
+
+    lower_left_corner = vec3([-2.0, -1.0, -1.0])
+    horizontal = vec3([4.0, 0.0, 0.0])
+    vertical = vec3([0.0, 2.0, 0.0])
+    origin = vec3([0.0, 0.0, 0.0])
+
+    points = []
+
+    for j in range(ny, 0, -1):
+        for i in range(nx):
+
+            u = i / nx
+            v = j / ny
+
+            ray = Ray(origin, lower_left_corner + u*horizontal + v*vertical)
+            col = blueBlend(ray)
+
+            ir = scaleColor(col[0])
+            ig = scaleColor(col[1])
+            ib = scaleColor(col[2])
+
+            points.append([ir, ig, ib])
+
+    ppmDrawer.writePpm(points)
 
 
 def paintColorGradient():
@@ -14,7 +53,7 @@ def paintColorGradient():
     points = []
 
     for j in range(ny, 0, -1):
-        for i in range (nx):
+        for i in range(nx):
             r = i / nx
             g = j / ny
             b = 0.2
@@ -27,23 +66,7 @@ def paintColorGradient():
 
 
 def main():
-    nx = 200
-    ny = 100
-    ppmDrawer = PpmDrawer("color_gradient.ppm", nx, ny)
-
-    points = []
-
-    for j in range(ny, 0, -1):
-        for i in range (nx):
-            r = i / nx
-            g = j / ny
-            b = 0.2
-            ir = scaleColor(r)
-            ig = scaleColor(g)
-            ib = scaleColor(b)
-            points.append([ir, ig, ib])
-
-    ppmDrawer.writePpm(points)
+    paintBlueGradient()
 
 
 if __name__ == "__main__":
