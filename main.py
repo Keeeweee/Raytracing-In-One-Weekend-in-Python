178ddba562ddcd742c81902f98dc415a360d8571
Keewee <epsilonmajorquezero@gmail.com>
2019-05-09 15:18:19 +0200

    Remove old functions

--- main.py
+++ main.py
@@ -19,69 +19,6 @@ def blueBlend(ray: Ray) -> vec3:
     return (1.0 - t) * vec3([1.0, 1.0, 1.0]) + t * vec3([0.5, 0.7, 1.0])
 
 
-def paintBlueGradient():
-    nx = 200
-    ny = 100
-    ppmDrawer = PpmDrawer("blue_gradient.ppm", nx, ny)
-
-    lower_left_corner = vec3([-2.0, -1.0, -1.0])
-    horizontal = vec3([4.0, 0.0, 0.0])
-    vertical = vec3([0.0, 2.0, 0.0])
-    origin = vec3([0.0, 0.0, 0.0])
-
-    points = []
-
-    for j in range(ny, 0, -1):
-        for i in range(nx):
-
-            u = i / nx
-            v = j / ny
-
-            ray = Ray(origin, lower_left_corner + u*horizontal + v*vertical)
-            col = blueBlend(ray)
-
-            ir = scaleColor(col[0])
-            ig = scaleColor(col[1])
-            ib = scaleColor(col[2])
-
-            points.append([ir, ig, ib])
-
-    ppmDrawer.writePpm(points)
-
-
-def paintColorGradient():
-    nx = 200
-    ny = 100
-    ppmDrawer = PpmDrawer("color_gradient.ppm", nx, ny)
-
-    points = []
-
-    for j in range(ny, 0, -1):
-        for i in range(nx):
-            r = i / nx
-            g = j / ny
-            b = 0.2
-            ir = scaleColor(r)
-            ig = scaleColor(g)
-            ib = scaleColor(b)
-            points.append([ir, ig, ib])
-
-    ppmDrawer.writePpm(points)
-
-
-def hitSphere(center: vec3, radius: float, ray: Ray) -> float:
-    oc = ray.origin - center
-    a = ray.direction.dot(ray.direction)
-    b = 2.0 * (oc.dot(ray.direction))
-    c = oc.dot(oc) - (radius**2)
-    discriminant = (b**2) - (4 * a * c)
-
-    if discriminant < 0:
-        return -1
-    else:
-        return (-b - sqrt(discriminant)) / (2 * a)
-
-
 def colorRay(ray: Ray, world: Shape) -> vec3:
     rec = [HitRecord()]
     if world.hit(ray, 0, 1000, rec):
