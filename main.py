a647d5b6e274314c90786b637d7cfee1b85fade6
Keewee <epsilonmajorquezero@gmail.com>
2019-05-09 16:48:35 +0200

    First failed attempt

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from Ray import Ray
 from Camera import Camera
 from random import random
 from Utils import randomInUnitSphere
+import multiprocessing as mp
 
 
 def scaleColor(color) -> int:
@@ -27,46 +28,52 @@ def colorRay(ray: Ray, world: Shape) -> vec3:
     return blueBlend(ray)
 
 
+nx = 200
+ny = 100
+ns = 100
+camera = Camera()
+
+points = []
+
+world = ShapeList()
+world.append(Sphere(vec3([0.0, 0.0, -1.0]), 0.5))
+world.append(Sphere(vec3([0.0, -100.5, -1.0]), 100))
+count = 0
+last = 0.0
+
+def run(j: int):
+    global count
+    global last
+    for i in range(nx):
+
+        new = int((count * 100) / (nx * ny))
+        if last != new:
+            print("Progress: " + str(last) + "% with j= " + str(j), end="\n")
+            last = new
+        count += 1
+
+        col = vec3([0.0, 0.0, 0.0])
+        for s in range(ns):
+            u = (i + random()) / nx
+            v = (j + random()) / ny
+
+            ray = camera.getRay(u, v)
+            col += colorRay(ray, world)
+
+        col = col / ns
+        ir = scaleColor(col[0])
+        ig = scaleColor(col[1])
+        ib = scaleColor(col[2])
+
+        points.append([ir, ig, ib])
+
+
 def paintWorld():
-    nx = 200
-    ny = 100
-    ns = 100
-    ppmDrawer = PpmDrawer("normals_world_aa.ppm", nx, ny)
+    ppmDrawer = PpmDrawer("08_parallel.ppm", nx, ny)
 
-    camera = Camera()
-
-    points = []
-
-    world = ShapeList()
-    world.append(Sphere(vec3([0.0, 0.0, -1.0]), 0.5))
-    world.append(Sphere(vec3([0.0, -100.5, -1.0]), 100))
-
-    count = 0
-    last = 0
-
-    for j in range(ny, 0, -1):
-        for i in range(nx):
-
-            new = int((count * 100) / (nx * ny))
-            if last != new:
-                print("Progress: " + str(last) + "%", end="\r")
-                last = new
-            count += 1
-
-            col = vec3([0.0, 0.0, 0.0])
-            for s in range(ns):
-                u = (i + random()) / nx
-                v = (j + random()) / ny
-
-                ray = camera.getRay(u, v)
-                col += colorRay(ray, world)
-
-            col = col / ns
-            ir = scaleColor(col[0])
-            ig = scaleColor(col[1])
-            ib = scaleColor(col[2])
-
-            points.append([ir, ig, ib])
+    N = mp.cpu_count()
+    with mp.Pool(processes= int(N)) as p:
+        p.map(run, [j for j in range(ny, 0, -1)])
 
     ppmDrawer.writePpm(points)
 
