c0d93fb65d70df1f6aec7dbb8e9eb39aedb9c951
Keewee <epsilonmajorquezero@gmail.com>
2019-05-09 10:44:44 +0200

    Add function to draw a red sphere

--- main.py
+++ main.py
@@ -65,8 +65,55 @@ def paintColorGradient():
     ppmDrawer.writePpm(points)
 
 
+def paintSphere():
+    nx = 200
+    ny = 100
+    ppmDrawer = PpmDrawer("red_sphere.ppm", nx, ny)
+
+    lower_left_corner = vec3([-2.0, -1.0, -1.0])
+    horizontal = vec3([4.0, 0.0, 0.0])
+    vertical = vec3([0.0, 2.0, 0.0])
+    origin = vec3([0.0, 0.0, 0.0])
+
+    points = []
+
+    for j in range(ny, 0, -1):
+        for i in range(nx):
+
+            u = i / nx
+            v = j / ny
+
+            ray = Ray(origin, lower_left_corner + u*horizontal + v*vertical)
+            col = colorRay(ray)
+
+            ir = scaleColor(col[0])
+            ig = scaleColor(col[1])
+            ib = scaleColor(col[2])
+
+            points.append([ir, ig, ib])
+
+    ppmDrawer.writePpm(points)
+
+
+def hitSphere(center: vec3, radius: float, ray: Ray) -> bool:
+    oc = ray.origin - center
+    a = ray.direction.dot(ray.direction)
+    b = 2.0 * (oc.dot(ray.direction))
+    c = oc.dot(oc) - radius * radius
+    discriminant = (b**2) - (4 * a * c)
+
+    return discriminant > 0
+
+
+def colorRay(ray: Ray) -> vec3:
+    if hitSphere(vec3([0, 0, -1]), 0.5, ray):
+        return vec3([1, 0, 0])
+
+    return blueBlend(ray)
+
+
 def main():
-    paintBlueGradient()
+    paintSphere()
 
 
 if __name__ == "__main__":
