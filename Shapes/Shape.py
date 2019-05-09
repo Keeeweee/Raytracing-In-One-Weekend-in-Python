70def2d3aec4e87bf7329450e46aaf1ab0891609
Keewee <epsilonmajorquezero@gmail.com>
2019-05-09 18:17:24 +0200

    Add Material to HitRecord

--- Shapes/Shape.py
+++ Shapes/Shape.py
@@ -2,13 +2,15 @@ from abc import ABC, abstractmethod
 from Ray import Ray
 from pyrr import Vector3 as vec3
 from typing import List
+import Materials.Material as Material
 
 
 class HitRecord:
-    def __init__(self, t: float = None, p: vec3 = None, normal: vec3 = None):
+    def __init__(self, t: float = None, p: vec3 = None, normal: vec3 = None, material: Material = None):
         self.t = t
         self.p = p
         self.normal = normal
+        self.material = material
 
 
 class Shape(ABC):
