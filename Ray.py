517750394a392cecd352c39a13947c33bc699ded
Keewee <epsilonmajorquezero@gmail.com>
2019-05-15 13:07:42 +0200

    Fix division by 0 when normalizing Ray directions

--- Ray.py
+++ Ray.py
@@ -4,7 +4,7 @@ from pyrr import vector3 as vec3
 class Ray:
     def __init__(self, origin: vec3, direction: vec3):
         self._origin = origin
-        self._direction = direction.normalized
+        self._direction = direction.normalized if direction.length > 0 else direction
 
     @property
     def origin(self) -> vec3:
@@ -19,4 +19,4 @@ class Ray:
 
     def copyFrom(self, copy):
         self._origin = copy.origin
-        self._direction = copy.direction.normalized
+        self._direction = copy.direction.normalized if copy.direction.length > 0 else copy.direction
