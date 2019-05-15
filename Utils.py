658b70ea65012e8c918a0a8b38df462feb0381a0
Keewee <epsilonmajorquezero@gmail.com>
2019-05-15 13:16:55 +0200

    Fix randomInUnitSphere getting points only outside of the unit sphere
    This actually improved not only the images but also performance since finding points in the unit cube inside the unit sphere is actually pretty much more probable that finding them outside.

--- Utils.py
+++ Utils.py
@@ -5,5 +5,6 @@ from random import random
 def randomInUnitSphere():
     while True:
         p = 2.0 * vec3([random(), random(), random()]) - vec3([1.0, 1.0, 1.0])
-        if p.squared_length >= 1.0:
+        if p.squared_length < 1.0:
+            return p
             return p
