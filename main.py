3269b01cf12a4418e06a98d3d5981ad54d30c407
Keewee <epsilonmajorquezero@gmail.com>
2019-05-16 09:56:20 +0200

    Add function to generate the graduation image

--- main.py
+++ main.py
@@ -37,40 +37,42 @@ def colorRay(ray: Ray, world: Shape, depth: int) -> vec3:
 
 
 def paintWorld():
-    ppmDrawer = PpmDrawer("testasd.ppm", nx, ny)
+    ppmDrawer = PpmDrawer("graduation.ppm", nx, ny)
 
     lookFrom = vec3([3.0, 3.0, 2.0])
     lookAt = vec3([0.0, 0.0, -1.0])
     vUp = vec3([0.0, 1.0, 0.0])
     distToFocus = (lookFrom - lookAt).length
     aperture = 2.0
-    camera = Camera(lookFrom, lookAt, vUp, 90.0/4.0, float(nx)/float(ny), aperture, distToFocus)
+    camera = Camera(lookFrom, lookAt, vUp, 90.0 / 4.0, float(nx) / float(ny), aperture, distToFocus)
 
     points = []
 
-    R = cos(pi/4.0)
-    world = ShapeList()
+    # world = ShapeList()
     # world.append(Sphere(vec3([-R, 0.0, -1.0]),
     #                     R,
     #                     Lambertian(vec3([0.0, 0.0, 1.0]))))
     # world.append(Sphere(vec3([R, 0.0, -1.0]),
     #                     R,
     #                     Lambertian(vec3([1.0, 0.0, 0.0]))))
-    world.append(Sphere(vec3([0.0, 0.0, -1.0]),
-                        0.5,
-                        Lambertian(vec3([0.1, 0.2, 0.5]))))
-    world.append(Sphere(vec3([0.0, -100.5, -1.0]),
-                        100,
-                        Lambertian(vec3([0.8, 0.8, 0.0]))))
-    world.append(Sphere(vec3([1.0, 0.0, -1.0]),
-                        0.5,
-                        Metal(vec3([0.8, 0.6, 0.2]), 0.3)))
-    world.append(Sphere(vec3([-1.0, 0.0, -1.0]),
-                        0.5,
-                        Dielectric(1.5)))
-    world.append(Sphere(vec3([-1.0, 0.0, -1.0]),
-                        -0.45,
-                        Dielectric(1.5)))
+
+    # world.append(Sphere(vec3([0.0, 0.0, -1.0]),
+    #                     0.5,
+    #                     Lambertian(vec3([0.1, 0.2, 0.5]))))
+    # world.append(Sphere(vec3([0.0, -100.5, -1.0]),
+    #                     100,
+    #                     Lambertian(vec3([0.8, 0.8, 0.0]))))
+    # world.append(Sphere(vec3([1.0, 0.0, -1.0]),
+    #                     0.5,
+    #                     Metal(vec3([0.8, 0.6, 0.2]), 0.3)))
+    # world.append(Sphere(vec3([-1.0, 0.0, -1.0]),
+    #                     0.5,
+    #                     Dielectric(1.5)))
+    # world.append(Sphere(vec3([-1.0, 0.0, -1.0]),
+    #                     -0.45,
+    #                     Dielectric(1.5)))
+
+    world = graduation()
 
     count = 0
     last = 0
@@ -97,6 +99,36 @@ def paintWorld():
             points.append(col)
 
     ppmDrawer.writePpm(points)
+
+
+def graduation():
+    world = ShapeList()
+    world.append(Sphere(vec3([0.0, -1000.0, 0]),
+                        1000.0,
+                        Lambertian(vec3([0.5, 0.5, 0.5]))))
+    for a in range(-11, 11):
+        for b in range(-11, 11):
+            chooseMat = random()
+            center = vec3([a + 0.9 * random(), 0.2, b + 0.9 * random()])
+            if (center - vec3([4.0, 0.2, 0.0])).length > 0.9:
+                if chooseMat < 0.8:  # Lambertian
+                    world.append(Sphere(center,
+                                        0.2,
+                                        Lambertian(
+                                            vec3([random() * random(), random() * random(), random() * random()]))))
+                elif chooseMat < 0.95:  # Metal
+                    world.append(Sphere(center,
+                                        0.2,
+                                        Metal(
+                                            vec3([1+random(), 1+random(), 1+random()]), 0.5*random())))
+                else:  # Glass
+                    world.append(Sphere(center, 0.2, Dielectric(1.5)))
+
+    world.append(Sphere(vec3([0.0, 1.0, 0.0]), 1.0, Dielectric(1.5)))
+    world.append(Sphere(vec3([-4.0, 1.0, 0.0]), 1.0, Lambertian(vec3([0.4, 0.2, 0.1]))))
+    world.append(Sphere(vec3([4.0, 1.0, 0.0]), 1.0, Metal(vec3([0.7, 0.6, 0.5]), 0.0)))
+
+    return world
 
 
 def main():
